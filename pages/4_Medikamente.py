554ffc8549e2f01dfb229d241011ccebc5fade1b
CGomes0 <131364111+CGomes0@users.noreply.github.com>
2023-05-06 20:41:29 +0200

    Add files via upload

---
+++ pages/4_Medikamente.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 18:04:36 2023
+
+@author: catar
+"""
+
+import streamlit as st
+import pandas as pd
+import json
+
+#Dataframe with pandas for medis
+with open("data.json", "r")as file: 
+    file_medi1 = json.load(file)
+
+medi1 = "medi1"
+Morgen = [day[medi1]for key, day in file_medi1.items() if medi1 in day]     #getting values from nested dictionary
+
+Mittag = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+
+Abend = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+
+Schlafen = [day[medi1]for key, day in file_medi1.items() if medi1 in day]
+
+Datum = file_medi1.keys()
+
+taken_medi = dict((k, eval(k)) for k in ("Morgen", "Mittag", "Abend", "Schlafen", "Datum"))     #creating dictionary for panda dataframe
+df = pd.DataFrame.from_dict(taken_medi, orient = "columns")
+
+st.dataframe(df, width=1024, height=768)
