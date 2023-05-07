05e9d7380d04850af668ba379360471ca2d4a738
CGomes0 <131364111+CGomes0@users.noreply.github.com>
2023-05-07 14:55:31 +0200

    Update 1__Menstruationsblutung.py

--- pages/1__Menstruationsblutung.py
+++ pages/1__Menstruationsblutung.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import json
+from jsonbin import load_data, save_data
+
+
+#jsonbin
+jsonbin_secrets = st.secrets["jsonbin"]
+api_key = jsonbin_secrets["api_key"]
+bin_id = jsonbin_secrets["bin_id"]
 
 
 st.title("Menstruationsblutung Auswertung")
 
 #barchart with pandas for Menstruationblutung
-with open("data.json", "r")as file:
-    file_intensity = json.load(file)
+file_intensity=load_data(apy_key, bin_id)
     
 intensity = "intensity"
 bleeding = [day[intensity]for key, day in file_intensity.items() if intensity in day]   #getting values from nested dictionary
