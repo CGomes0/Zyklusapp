90fe1e90d49928b4ee5d7c0ad7fe699a985abf87
CGomes0 <131364111+CGomes0@users.noreply.github.com>
2023-05-28 20:20:45 +0200

    Update 2_Schmerzen.py

--- pages/2_Schmerzen.py
+++ pages/2_Schmerzen.py
@@ -52,12 +52,12 @@ pain = "pain"
 pain_values = [day[pain]for key, day in file_pain.items() if pain in day]   #getting values from nested dictionary
 
 dif = pd.DataFrame({
-    "pain" : pain_values,
-    "day" : file_pain.keys()
+    "Intensität" : pain_values,
+    "Tag" : file_pain.keys()
     })
 
 #st.write("Gemessene Werte", pain_values)
 
-st.line_chart(dif, x= "day", y = "pain")
+st.line_chart(dif, x= "Tag", y = "Intensität")
 
 
